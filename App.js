acda63f2f78adc339515cf7f1b44ec3aece669df
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-10-02 19:26:21 +0300

    Kaljat saa haettua

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const[brand, setBrand] = useState('')
+  const[name, setName] = useState('')
+  const[style, setStyle] = useState('')
+
+  const url = 'https://random-data-api.com/api/v2/beers'
+
+  const getBeerData = async () => {
+    try {
+      const response = await fetch(url)
+      if (response.ok) {
+        console.log('Response ok')
+        const data = await response.json()
+        setBrand(data.brand)
+        setName(data.name)
+        setStyle(data.style)
+      } else {
+        if (response.status === 429) {
+          console.log('Too many requests')
+        } else {
+          console.log('Kaljat on loppu')
+        }
+      }
+
+    } catch (error) {
+      console.error(error)
+    } 
+  }
+
+  useEffect(() => {
+    getBeerData()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Random beer</Text>
+      <Text style={styles.text}>Brand: {brand}</Text>
+      <Text style={styles.text}>Name: {name}</Text>
+      <Text style={styles.text}>Style: {style}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +54,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10
+  }
 });
